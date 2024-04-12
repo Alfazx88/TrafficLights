e6747ac3c2d489058ec0f8905c0701de9d8004e5
Alfazx88 <alfazx@yandex.ru>
2024-04-12 21:16:18 +0200

    Initial UI setup

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var switchTrafficLightsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        switchTrafficLightsButton.layer.cornerRadius = 10
+        switchTrafficLightsButton.setTitle("Start", for: .normal)
+        redLightView.layer.cornerRadius = 55
+        yellowLightView.layer.cornerRadius = 55
+        greenLightView.layer.cornerRadius = 55
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchTrafficLightsDidTapped(_ sender: UIButton) {
+        
+        
+        
+        
+        
+    }
+    
 }
 
