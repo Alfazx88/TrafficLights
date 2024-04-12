bead0e8e5996987d2b117ae324724f54437b3bf6
Alfazx88 <alfazx@yandex.ru>
2024-04-12 21:27:10 +0200

    Alpha on views installed

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
-    
     @IBOutlet var switchTrafficLightsButton: UIButton!
     
     override func viewDidLoad() {
@@ -20,14 +19,18 @@ class ViewController: UIViewController {
         switchTrafficLightsButton.layer.cornerRadius = 10
         switchTrafficLightsButton.setTitle("Start", for: .normal)
         redLightView.layer.cornerRadius = 55
+        redLightView.alpha = 0.3
         yellowLightView.layer.cornerRadius = 55
+        yellowLightView.alpha = 0.3
         greenLightView.layer.cornerRadius = 55
+        greenLightView.alpha = 0.3
         
         
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func switchTrafficLightsDidTapped(_ sender: UIButton) {
+    @IBAction func switchTrafficLightsDidTapped() {
+        switchTrafficLightsButton.setTitle("Next", for: .normal)
         
         
         
